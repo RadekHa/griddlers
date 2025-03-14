38f41be55e82d81129c9563e34ee02167534865d
Radek Haščák <radek.hascak@gopascz.onmicrosoft.com>
2025-03-14 10:50:51 +0100

    Add uncrustify to project.

--- src/rule/rule.h
+++ src/rule/rule.h
@@ -1,7 +1,10 @@
 #pragma once
 
+#include <bitset>
+#include <cstdint>
 #include <cstdint>
 
-uint64_t fibonacci(uint64_t number);
 
-uint32_t factorial( uint32_t number );
+uint64_t fibonacci (uint64_t number);
+
+uint32_t factorial (uint32_t number);
